ece9eb368f249b7b01f2d1692f7181edcd9f8bd7
JesusKrastev <jesuskrastev@gmail.com>
2025-08-15 23:53:55 +0200

    Integrate navigation with NavHost and NavBar
    This commit introduces a navigation structure using `WatodoNavHost` and conditionally displays a `NavBar` based on the current route.
    
    - **app/src/main/java/com/jesuskrastev/watodo/MainActivity.kt:**
        - Replaced the direct rendering of `ActivitiesScreen` with `WatodoNavHost` to enable navigation between different screens.
        - The `NavBar` is now conditionally displayed, hidden when the current route is `LoginRoute`.
        - `navController` and `currentBackStackEntryAsState` are used to manage navigation state and determine the current route.

--- app/src/main/java/com/jesuskrastev/watodo/MainActivity.kt
+++ app/src/main/java/com/jesuskrastev/watodo/MainActivity.kt
@@ -1,24 +1,22 @@
 package com.jesuskrastev.watodo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContent
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.jesuskrastev.watodo.ui.features.activities.ActivitiesScreen
-import com.jesuskrastev.watodo.ui.features.activities.ActivitiesState
-import com.jesuskrastev.watodo.ui.features.activities.ActivitiesViewModel
-import com.jesuskrastev.watodo.ui.theme.WatodoTheme
-import androidx.compose.runtime.getValue
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.jesuskrastev.watodo.ui.features.components.NavBar
+import com.jesuskrastev.watodo.ui.navigation.LoginRoute
+import com.jesuskrastev.watodo.ui.navigation.WatodoNavHost
+import com.jesuskrastev.watodo.ui.theme.WatodoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,19 +26,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WatodoTheme {
-                val vm: ActivitiesViewModel = hiltViewModel()
-                val state: ActivitiesState by vm.state.collectAsStateWithLifecycle(initialValue = ActivitiesState())
+                val navController = rememberNavController()
+                val navBackStackEntry = navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry.value?.destination
 
                 Scaffold(
                     contentWindowInsets = WindowInsets.navigationBars,
                     bottomBar = {
-                        NavBar()
+                        if (currentRoute?.hasRoute(LoginRoute::class) == false) NavBar()
                     }
                 ) { paddingValues ->
-                    ActivitiesScreen(
+                    WatodoNavHost(
                         modifier = Modifier.padding(paddingValues),
-                        state = state,
-                        onEvent = vm::onEvent,
+                        navController = navController,
                     )
                 }
             }
