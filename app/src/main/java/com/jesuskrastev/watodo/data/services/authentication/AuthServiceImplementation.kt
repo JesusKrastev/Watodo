fdf463b507496f136a8c0ac70c76ea17e31a3847
JesusKrastev <jesuskrastev@gmail.com>
2025-08-17 22:07:41 +0200

    Implement auth state listeners
    This commit introduces functionality to observe the Firebase authentication state.
    
    - **app/src/main/java/com/jesuskrastev/watodo/data/services/authentication/AuthServiceImplementation.kt:**
        - Added `observeUser()`: A `Flow` that emits the current `FirebaseUser` or `null` whenever the authentication state changes.
        - Added `observeUserLoggedIn()`: A `Task<Boolean>` that completes with `true` when a user logs in. The listener is removed upon completion.

--- app/src/main/java/com/jesuskrastev/watodo/data/services/authentication/AuthServiceImplementation.kt
+++ app/src/main/java/com/jesuskrastev/watodo/data/services/authentication/AuthServiceImplementation.kt
@@ -5,12 +5,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.TaskCompletionSource
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.jesuskrastev.watodo.R
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
@@ -19,6 +23,33 @@ class AuthServiceImplementation @Inject constructor(
     @ApplicationContext private val context: Context,
 ): AuthService {
     private val logTag: String = "AuthServiceImplementation"
+
+    fun observeUser(): Flow<FirebaseUser?> = callbackFlow {
+        val listener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            trySend(firebaseAuth.currentUser) // emite null si no hay usuario
+        }
+        firebaseAuth.addAuthStateListener(listener)
+
+        awaitClose { firebaseAuth.removeAuthStateListener(listener) }
+    }
+
+    fun observeUserLoggedIn(): Task<Boolean> {
+        val taskCompletionSource = TaskCompletionSource<Boolean>()
+        var authStateListener: FirebaseAuth.AuthStateListener? = null
+
+        authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user: FirebaseUser? = firebaseAuth.currentUser
+            if (user != null) taskCompletionSource.setResult(true)
+        }
+
+        firebaseAuth.addAuthStateListener(authStateListener)
+
+        taskCompletionSource.task.addOnCompleteListener {
+            firebaseAuth.removeAuthStateListener(authStateListener)
+        }
+
+        return taskCompletionSource.task
+    }
 
     fun getUser(): FirebaseUser? = firebaseAuth.currentUser
 
