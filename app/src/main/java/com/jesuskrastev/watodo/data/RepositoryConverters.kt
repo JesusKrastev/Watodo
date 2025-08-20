8fdd8ddc79268bb10d13aa9a1dbd7ea7c7f1bcf1
JesusKrastev <jesuskrastev@gmail.com>
2025-08-20 20:49:09 +0200

    Add Web model and Firestore converter
    This commit introduces a new `Web` data model and a corresponding converter function to transform `WebFirestore` objects into `Web` objects.
    
    - **app/src/main/java/com/jesuskrastev/watodo/data/RepositoryConverters.kt:**
        - Added `toWeb()` extension function to convert `WebFirestore` to `Web`, including parsing the URL string to `android.net.Uri`.

--- app/src/main/java/com/jesuskrastev/watodo/data/RepositoryConverters.kt
+++ app/src/main/java/com/jesuskrastev/watodo/data/RepositoryConverters.kt
@@ -1,10 +1,21 @@
 package com.jesuskrastev.watodo.data
 
+import android.net.Uri
 import com.jesuskrastev.watodo.data.firestore.activity.ActivityFirestore
 import com.jesuskrastev.watodo.data.firestore.user.UserFirestore
+import com.jesuskrastev.watodo.data.firestore.web.WebFirestore
 import com.jesuskrastev.watodo.data.room.activity.ActivityEntity
 import com.jesuskrastev.watodo.models.Activity
 import com.jesuskrastev.watodo.models.User
+import com.jesuskrastev.watodo.models.Web
+
+fun WebFirestore.toWeb(): Web =
+    Web(
+        id = id,
+        title = title,
+        description = description,
+        url = Uri.parse(url),
+    )
 
 fun ActivityFirestore.toActivity(): Activity =
     Activity(
